793123fb404b8936b405e73a4ead07a09af83af2
arcinth <arcinth03@gmail.com>
2026-01-12 13:35:28 +0530

    mettl upload

---
+++ IS_EVEN.java
@@ -0,0 +1,16 @@
+class IS_EVEN{
+    public static void main(String[] args) {
+        int num = 5;
+        int result =findeven(num);
+        System.out.println(result);
+ 
+    }
+    static int findeven(int num){
+        if(num%2==0){
+            return 2;
+        }
+        else{
+            return 1;
+        }
+    }
+}
